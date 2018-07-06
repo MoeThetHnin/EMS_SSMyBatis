63ed8e839ceab280987e32de8107dcf6d6217b62
Maw Paing Thu <paingmawthu@gmail.com>
2018-07-06 13:07:21 +0900

    ems_mpt_window_20180706_01

--- src/main/java/com/ansur/ems_ansur_ssmybatis/actions/FunctionAction.java
+++ src/main/java/com/ansur/ems_ansur_ssmybatis/actions/FunctionAction.java
@@ -2,6 +2,7 @@ package com.ansur.ems_ansur_ssmybatis.actions;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,151 +30,112 @@ import com.itextpdf.text.pdf.PdfWriter;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class FunctionAction {
-	
+
 	private Function function;
-	
+
 	String filename;
-	
+
 	private String filePath;
-	
+
 	private FileInputStream fileInputStream;
-	
+
+	private FileInputStream fs;
+
 	private FileOutputStream fos;
-	
+
 	private ZipOutputStream zos;
-	
+
 	public String excelToPdf() throws IOException, DocumentException {
 		int columnWidth[] = { 3000, 2500, 6000, 5000, 3000, 1500, 3000, 2000, 2000, 2000, 6000 };
-		String fontPath = getFilePath()+"\\Fonts\\sazanami-mincho.ttf";
-		
-		for(int i=0; i<function.getUploadImage().length;i++) {
+		String fontPath = getFilePath() + "\\Fonts\\sazanami-mincho.ttf";
+
+		for (int i = 0; i < function.getUploadImage().length; i++) {
 			FileInputStream inputDocument = new FileInputStream(function.getUploadImage()[i]);
-			
+
 			HSSFWorkbook workBook = new HSSFWorkbook(inputDocument);
 			Document newPdf = new Document(PageSize.A4.rotate());
-			
+
 			BaseFont bf;
 			Font font;
-			bf = BaseFont.createFont(fontPath,BaseFont.IDENTITY_H,BaseFont.EMBEDDED);
-			font = new Font(bf,10);
-			
-			PdfWriter.getInstance(newPdf, new FileOutputStream(getFilePath()+"\\PdfFiles\\"+function.getUploadImageFileName()[i].split("\\.")[0]+".pdf"));
+			bf = BaseFont.createFont(fontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+			font = new Font(bf, 10);
+
+			PdfWriter.getInstance(newPdf, new FileOutputStream(
+					getFilePath() + "\\PdfFiles\\" + function.getUploadImageFileName()[i].split("\\.")[0] + ".pdf"));
 			newPdf.open();
 			PdfPTable table = new PdfPTable(11);
 			table.setWidths(columnWidth);
 			PdfPCell table_cell;
-			/*for(int i=0;i<xlsWB.getNumberOfSheets();i++) {*/
-				HSSFSheet xlsSheet = workBook.getSheetAt(0);
-				
-				Iterator<Row> rowIterator = xlsSheet.iterator();
-				
-				while(rowIterator.hasNext()) {
-					Row row = rowIterator.next();
-					Iterator<Cell> cellIterator = row.cellIterator();
-					while(cellIterator.hasNext()){
-						Cell cell = cellIterator.next();
-						switch(cell.getCellType()) {
-						case Cell.CELL_TYPE_STRING:
-							table_cell = new PdfPCell(new Phrase(cell.getStringCellValue(),font));
-							table.addCell(table_cell);
-							break;
-						case Cell.CELL_TYPE_NUMERIC:
-							table_cell = new PdfPCell(new Phrase((float) cell.getNumericCellValue()));
-							table.addCell(table_cell);
-							break;						
-						}
-					}
-				}		
-			
-			
-			newPdf.add(table);
-		
-			newPdf.close();
-			inputDocument.close();
-			
-		
-	
-		}
-		
-		/*FileInputStream inputDocument = new FileInputStream(getFunction().getUploadImage());
-		
-		HSSFWorkbook workBook = new HSSFWorkbook(inputDocument);
-		Document newPdf = new Document(PageSize.A4.rotate());
-		
-		BaseFont bf;
-		Font font;
-		bf = BaseFont.createFont(fontPath,BaseFont.IDENTITY_H,BaseFont.EMBEDDED);
-		font = new Font(bf,10);
-		PdfWriter.getInstance(newPdf, new FileOutputStream(getFilePath()+"\\PdfFiles\\"+getFunction().getUploadImageFileName().split("\\.")[0]+".pdf"));
-		newPdf.open();
-		PdfPTable table = new PdfPTable(11);
-		table.setWidths(columnWidth);
-		PdfPCell table_cell;
-		for(int i=0;i<xlsWB.getNumberOfSheets();i++) {
+			/* for(int i=0;i<xlsWB.getNumberOfSheets();i++) { */
 			HSSFSheet xlsSheet = workBook.getSheetAt(0);
-			
+
 			Iterator<Row> rowIterator = xlsSheet.iterator();
-			
-			while(rowIterator.hasNext()) {
+
+			while (rowIterator.hasNext()) {
 				Row row = rowIterator.next();
 				Iterator<Cell> cellIterator = row.cellIterator();
-				while(cellIterator.hasNext()){
+				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
-					switch(cell.getCellType()) {
+					switch (cell.getCellType()) {
 					case Cell.CELL_TYPE_STRING:
-						table_cell = new PdfPCell(new Phrase(cell.getStringCellValue(),font));
+						table_cell = new PdfPCell(new Phrase(cell.getStringCellValue(), font));
 						table.addCell(table_cell);
 						break;
 					case Cell.CELL_TYPE_NUMERIC:
 						table_cell = new PdfPCell(new Phrase((float) cell.getNumericCellValue()));
 						table.addCell(table_cell);
-						break;						
+						break;
 					}
 				}
-			}		
-		
-		
-		newPdf.add(table);
-	
-		newPdf.close();
-		inputDocument.close();*/
-		
-		
-		// 作成したエクセルファイルをダウンロードする
-		/*		fileInputStream = new FileInputStream(new File(getFilePath() + "\\PdfFiles\\" +getFunction().getUploadImageFileName().split("\\.")[0]+ ".pdf"));
-				filename = getFunction().getUploadImageFileName().split("\\.")[0] + ".pdf";// ダウンロードファイル名前のなめにstruts.xmlに送ること
-*/		
-		String zipFile = getFilePath()+"\\ZipFiles\\test.zip";
-		int totalFiles = function.getUploadImageFileName().length;
-		String srcFiles[] =new String [totalFiles];  
-		
-		
-		byte[] buffer = new byte[2048];
-		
-		fos = new FileOutputStream(zipFile);
-		
-		zos = new ZipOutputStream(fos);
-		
-		for(int i=0;i<srcFiles.length;i++) {
-			File srcFile = new File(getFilePath()+"\\PdfFiles\\"+function.getUploadImageFileName()[i].split("\\.")[0]+".pdf");
-			fileInputStream = new FileInputStream(srcFile);
-			zos.putNextEntry(new ZipEntry(srcFile.getName()));
-			
-			int length;
-			while((length=fileInputStream.read(buffer))>0) {
-				zos.write(buffer,0,length);
 			}
+
+			newPdf.add(table);
+
+			newPdf.close();
+			inputDocument.close();
+
+		}
+
+		crateZipFile();
+
+		// Zip ファイルをダウンロードすること
+		
+		  File dfile = new File(getFilePath()+"\\ZipFiles\\pdfFiles.zip"); 
+		  fileInputStream = new FileInputStream(dfile); 
+		  filename = dfile.getName();
+		 
+
+		return ActionSupport.SUCCESS;
+	}
+
+	private void crateZipFile() throws IOException {
+		// TODO Auto-generated method stub
+
+		fos = new FileOutputStream(getFilePath() + "\\ZipFiles\\pdfFiles.zip");
+		zos = new ZipOutputStream(fos);
+		for (int i = 0; i < function.getUploadImage().length; i++) {
+			String pdfFilePath = getFilePath() + "\\PdfFiles\\" + function.getUploadImageFileName()[i].split("\\.")[0]
+					+ ".pdf";
+			System.out.println(pdfFilePath);
+			File f = new File(pdfFilePath);
+			fileInputStream = new FileInputStream(f);
+			ZipEntry zipEntry = new ZipEntry(function.getUploadImageFileName()[i].split("\\.")[0] + ".pdf");
+			zos.putNextEntry(zipEntry);
+
+			byte[] bytes = new byte[1024];
+			int length;
+			while ((length = fileInputStream.read(bytes)) >= 0) {
+				zos.write(bytes, 0, length);
+			}
+
 			zos.closeEntry();
 			fileInputStream.close();
 		}
-		
-		File dfile = new File(getFilePath()+"\\ZipFiles\\test.zip");
-		fileInputStream = new FileInputStream(dfile);
-		filename = "test.zip";
-		
-		return ActionSupport.SUCCESS;
+		zos.close();
+		fos.close();
+
 	}
-	
+
 	private String getFilePath() {
 		Properties p = new Properties();
 		try {
@@ -206,15 +168,12 @@ public class FunctionAction {
 		return fileInputStream;
 	}
 
-
-
 	public ZipOutputStream getZos() {
 		return zos;
 	}
 
+	public FileInputStream getFs() {
+		return fs;
+	}
 
-	
-	
-	
-	
 }
--- src/main/webapp/WEB-INF/home/welcome.jsp
+++ src/main/webapp/WEB-INF/home/welcome.jsp
@@ -5,56 +5,65 @@
 <!DOCTYPE html>
 <html>
 <body>
-	
+
 	<div style="width: 100%;">
 		<div style="text-align: center;">
 			<h2>株式会社アンスール</h2>
 			<h3>従業員管理システムアドミン側</h3>
 		</div>
-		<div style="width:80%; margin:1% auto;">
-			<a style="float:right; color:gray; text-decoration: none;" href="<s:url action="logout.action"/>"> ログアウト</a>
+		<div style="width: 80%; margin: 1% auto;">
+			<a style="float: right; color: gray; text-decoration: none;"
+				href="<s:url action="logout.action"/>"> ログアウト</a>
 		</div>
-		<div style="width: 80%; margin: 1% auto; border: 1px solid gray; clear:right;">
+		<div
+			style="width: 80%; margin: 1% auto; border: 1px solid gray; clear: right;">
 			<a href="<s:url action="empManage.action"/>"
-				style="text-decoration: none; color:gray;">
-				<div style="width: 21%; margin: 1% 1% 1% 3%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
+				style="text-decoration: none; color: gray;">
+				<div
+					style="width: 21%; margin: 1% 1% 1% 3%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
 					<h4>社員管理</h4>
 				</div>
+			</a> <a href="<s:url action="departmentManage.action"/>"
+				style="text-decoration: none; color: gray;">
+				<div
+					style="width: 21%; margin: 1% 1% 1% 2%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
+					<h4>部門管理と役職管理</h4>
+				</div>
+			</a> <a href="<s:url action="ekiManage.action"/>"
+				style="text-decoration: none; color: gray;">
+				<div
+					style="width: 21%; margin: 1% 1% 1% 2%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
+					<h4>駅管理</h4>
+				</div>
+			</a> <a href="#" style="text-decoration: none; color: gray;">
+				<div
+					style="width: 21%; margin: 1% 1% 1% 2%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
+					<h4>OO管理</h4>
+				</div>
 			</a>
-			<a href="<s:url action="departmentManage.action"/>"
-				style="text-decoration: none; color:gray;">
-			<div
-				style="width: 21%; margin: 1% 1% 1% 2%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
-				<h4>部門管理と役職管理</h4>
-			</div>
-			</a>
-			
-			<a href="<s:url action="ekiManage.action"/>"
-				style="text-decoration: none; color:gray;">
-			<div
-				style="width: 21%; margin: 1% 1% 1% 2%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
-				<h4>駅管理</h4>
-			</div>
-			</a>
-			<a href="#"
-				style="text-decoration: none; color:gray;">
-			<div
-				style="width: 21%; margin: 1% 1% 1% 2%; border: 1px solid gray; background-color: #f1f1f1; text-align: center; float: left;">
-				<h4>OO管理</h4>
-			</div>
-			</a>
-			
-			
+
+
 		</div>
-			<div style="width: 80%; margin: 1% auto; border: 1px solid gray; clear:left;">
+		<div
+			style="width: 80%; margin: 1% auto;clear: left;">
+			<div style="width: 50%;float:left;">
+
 				<h4 style="text-align: center;">コンバータ</h4>
-				
-				<h5>エクセルファイルをPDFファイルに変更する機能　(.xls to .pdf)</h5>
-				<s:form action="excelToPdf" enctype="multipart/form-data">
-					<s:file name="function.uploadImage" multiple="multiple"></s:file>
-					<s:submit value="PDFファイルに変更"></s:submit>
-				</s:form>
+				<div style="clear:left; width: 90%; margin:1% auto; border: 1px solid gray;">
+
+					<h5 style="text-align: center;">エクセルファイルをPDFファイルに変更する機能 (.xls
+						to .pdf)</h5>
+					<div style="width: 50%; margin: 1% auto;">
+						<s:form action="excelToPdf" enctype="multipart/form-data">
+							<s:file name="function.uploadImage" multiple="multiple"></s:file>
+							<s:submit value="PDFファイルに変更"></s:submit>
+						</s:form>
+					</div>
+				</div>
+				<div style="clear:left; width: 90%; margin:1% auto; border: 1px solid gray;">
+					</div>
 			</div>
+		</div>
 	</div>
 
 	<p>
